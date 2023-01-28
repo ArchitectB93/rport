3fb73da469f6e80892149dec62e7c554650cf4aa
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-28 07:56:32 -0800

    sduigfalsdhf

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <footer className='py-5 bg-dark fixed-bottom'>
+                <div className='container'>
+                    <p class="m-0 text-center text-white">
+                        A react portfolio project
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
