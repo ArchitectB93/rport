63f05601ab0e9e7bd85e02f127017b0b793924c5
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-29 18:11:52 -0800

    asdfsdaf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
-        <div className='footer'>
+        <div className="footer">
             <footer className='py-5 bg-dark fixed-bottom'>
                 <div className='container'>
-                
+                    <a href='https://github.com/ArchitectB93' target="_blank" rel='noreferrer'>
+                        <button>
+                            <FontAwesomeIcon icon={faGithub} id="github"/>
+                        </button>
+                    </a>
                 </div>
             </footer>
         </div>
--- src/index.js
+++ src/index.js
@@ -13,20 +13,21 @@ import {
   Post,
 } from "./components";
 
-ReactDOM.render(
-<Router>
-  <Navigation />
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/blog" element={<Blog />}>
-      <Route path="" element={<Posts />} />
-      <Route path=":postSlug" element={<Post />} />
-    </Route>
-  </Routes>
-  <Footer />
-</Router>,
 
- document.getElementById("root")
+ReactDOM.render(
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
