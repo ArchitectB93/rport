9095e9370cfc3fd7ccada65a34a01f837b4e6af9
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-29 18:41:31 -0800

    adsfasdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import "./footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
-        <div className="footer">
+        <div className="footer" id='foot'>
             <footer className='py-5 bg-dark fixed-bottom'>
                 <div className='container'>
                     <a href='https://github.com/ArchitectB93' target="_blank" rel='noreferrer'>
                         <button>
                             <FontAwesomeIcon icon={faGithub} id="github"/>
+                        </button>
+                    </a>
+                    <a href='https://www.linkedin.com/in/architectb/' rel='noreferrer' target="_blank">
+                        <button>
+                            <FontAwesomeIcon icon={faLinkedin} id="link"/>
+                        </button>
+                    </a>
+                    <a href='https://architectb93.github.io/ab93.github.io/about.html' rel='noreferrer'>
+                        <button id="main">
+                            Main page
                         </button>
                     </a>
                 </div>
